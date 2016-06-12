393939f16842aa40278eea9a9132c082ea1c9e21
source201 <source201@163.com>
2016-06-12 17:27:37 +0800

    first

---
+++ teamtest/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(teamtest.Startup))]
+namespace teamtest
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
